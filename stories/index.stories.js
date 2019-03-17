1ec9255efce5af7d3051ebaabcc7fee6b399d180
Laurent Thiebault <thiebault.laurent@gmail.com>
2019-03-17 23:01:34 +0100

    :rocket: add storybook-deployer

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,12 +5,22 @@ import { storiesOf } from '@storybook/react';
 
 storiesOf('Slides/Intro', module)
   .addDecorator(storyFn => <StoryWrapper>{storyFn()}</StoryWrapper>)
-  .add('Hello', () => <h1>Hello Storybook <span role="img" aria-label="tada">🎉</span></h1>);
+  .add('Storybook', () => (
+    <div>
+      <h1>Storybook</h1>
+      <h1>The playground you need for your UI components! <span role="img" aria-label="tada">🎨</span></h1>
+    </div>
+  ));
 
 const StoryWrapper = styled.div`
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial;
   display: flex;
   justify-content: center;
   align-items: center;
+  text-align: center;
   height: 100vh;
+`;
+
+const Title = styled.div`
+  
 `;
