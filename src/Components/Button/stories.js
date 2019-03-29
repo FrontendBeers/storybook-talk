e15754fe7f8a3cfda40281cb396b979898bfd87d
Laurent Thiebault <thiebault.laurent@gmail.com>
2019-03-29 20:36:27 +0100

    :art: Add a11y addon

--- src/Components/Button/stories.js
+++ src/Components/Button/stories.js
@@ -4,11 +4,13 @@ import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
+import { withA11y } from '@storybook/addon-a11y';
 import Button from '.';
 
 storiesOf('Components/Button', module)
   .addDecorator(storyFn => <StoryWrapper>{storyFn()}</StoryWrapper>)
   .addDecorator(withInfo)
+  .addDecorator(withA11y)
   .add('Default', () => <Button>Click me</Button>)
   .add('With onClick', () => (
     <Button onClick={action('button-click')}>Click me</Button>
