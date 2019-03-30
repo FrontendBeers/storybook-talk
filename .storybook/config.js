4b70a7215fe4bda51926722d02637a26c8c4539c
Laurent Thiebault <thiebault.laurent@gmail.com>
2019-03-30 14:05:40 +0100

    :white_check_mark: Init storyshots

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure, addParameters } from '@storybook/react';
+import requireContext from 'require-context.macro';
 import COLORS from '../src/Utils/Colors';
 
 addParameters({
@@ -11,12 +12,8 @@ addParameters({
 });
 
 // automatically import all files ending in *.stories.js
-const reqSlides = require.context('../src/Slides', true, /.stories.js$/);
-const reqComponents = require.context(
-  '../src/Components',
-  true,
-  /.stories.js$/
-);
+const reqSlides = requireContext('../src/Slides', true, /.stories.js$/);
+const reqComponents = requireContext('../src/Components', true, /.stories.js$/);
 function loadStories() {
   reqSlides.keys().forEach(filename => reqSlides(filename));
   reqComponents.keys().forEach(filename => reqComponents(filename));
