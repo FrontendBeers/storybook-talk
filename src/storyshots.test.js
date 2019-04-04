69ba146e2cc2349f6a2374e9175c48094b78a4d3
Laurent Thiebault <thiebault.laurent@gmail.com>
2019-04-04 14:55:11 +0200

    :white_check_mark: Update snapshots

--- src/storyshots.test.js
+++ src/storyshots.test.js
@@ -1,3 +1,9 @@
 import initStoryshots from '@storybook/addon-storyshots';
 
+// eslint-disable-next-line
+jest.mock('@storybook/addon-info', () => ({
+  withInfo: () => storyFn => storyFn,
+  setDefaults: () => {}
+}));
+
 initStoryshots();
