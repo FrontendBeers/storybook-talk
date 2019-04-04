7cef8f9b16c6dc161b7f55ac654b27a25b79fa71
Laurent Thiebault <thiebault.laurent@gmail.com>
2019-04-04 13:35:34 +0200

    :art: Update app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Text from './Components/Text';
+import Button from './Components/Button';
 
-const App = () => <h1>Test</h1>;
+const App = () => {
+  const [logs, setLogs] = useState([]);
+
+  const getTwoDigits = time => `0${time}`.substr(-2);
+
+  const getTime = date =>
+    `${getTwoDigits(date.getHours())}:${getTwoDigits(
+      date.getMinutes()
+    )}:${getTwoDigits(date.getSeconds())}`;
+
+  return (
+    <Wrapper>
+      <Text size="40px">Welcome to my wonderful app!</Text>
+      <StyledButton onClick={() => setLogs([...logs, getTime(new Date())])}>
+        What time is it ?
+      </StyledButton>
+      <LogsWrapper>
+        {logs.map(log => (
+          <Text style={{ margin: 0, marginBottom: '10px' }}>{log}</Text>
+        ))}
+      </LogsWrapper>
+    </Wrapper>
+  );
+};
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledButton = styled(Button)`
+  margin: 0;
+  margin-bottom: 40px;
+  width: 300px;
+`;
+
+const LogsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
